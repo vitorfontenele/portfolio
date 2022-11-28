9d7cec64cc23fc88ea040af1722b63263cdfe317
vitor-fontenele <vitor_fontenele@hotmail.com>
2022-11-28 20:58:42 -0300

    Create header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import SectionAbout from "./components/SectionAbout/SectionAbout";
 import SectionProjects from "./components/SectionProjects/SectionProjects";
 import SectionHero from './components/SectionHero/SectionHero';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div id="content">
         <SectionHero />
         <SectionAbout />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header id="header">
+            <nav>
+                <ul id="nav-list">
+                    <li className="nav-item"><a href="#hero" className="nav-link"><span className="nav-item-number highlight-text">01. </span>Home</a></li>
+                    <li className="nav-item"><a href="#about" className="nav-link"><span className="nav-item-number highlight-text">02. </span>Sobre</a></li>
+                    <li className="nav-item"><a href="#projects" className="nav-link"><span className="nav-item-number highlight-text">03. </span>Projetos</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
